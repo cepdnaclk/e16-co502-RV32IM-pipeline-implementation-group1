fd823d66c0466df6c877074f6489274adc1b4db3
Buddhi Perera <gkbhperera.tck@gmail.com>
2021-07-22 15:43:55 +0530

    added control unit testing except for load and store

--- control_unit_module/control_unit_tb.v
+++ control_unit_module/control_unit_tb.v
@@ -179,6 +179,7 @@ module control_unit_tb;
         `assert(IMM_SEL,`B_TYPE);
         $display("BGEU test passed!");
 
+        // TODO: Need to design the load and store
         // LB
         // LH
         // LW
@@ -189,29 +190,149 @@ module control_unit_tb;
         // SW
 
         // ADDI
-        // OPCODE = `I_TYPE_OPCODE;
-        // FUNCT3 = 3'b000;
-        // FUNCT7 = 7'bxxxxxxx;
-        // `DECODE_DELAY
-        // `assert(OP1SEL,`DATA1);
-        // `assert(OP2SEL,`IMM);
-        // `assert(MEM_WRITE,`MEM_WRITE_0);
-        // `assert(MEM_READ,`MEM_READ_0);
-        // `assert(REG_WRITE_EN,`REG_WRITE_EN_1);
-        // `assert(WB_SEL,`ALU);
-        // `assert(ALU_OP,`ADD);
-        // `assert(BRANCH_JUMP,`NO);
-        // `assert(IMM_SEL,`I_SIGNED_TYPE);
-        // $display("ADDI test passed!");
+        OPCODE = `I_TYPE_OPCODE;
+        FUNCT3 = 3'b000;
+        FUNCT7 = 7'bxxxxxxx;
+        `DECODE_DELAY
+        `assert(OP1SEL,`DATA1);
+        `assert(OP2SEL,`IMM);
+        `assert(MEM_WRITE,`MEM_WRITE_0);
+        `assert(MEM_READ,`MEM_READ_0);
+        `assert(REG_WRITE_EN,`REG_WRITE_EN_1);
+        `assert(WB_SEL,`ALU);
+        `assert(ALU_OP,`ADD);
+        `assert(BRANCH_JUMP,`NO);
+        `assert(IMM_SEL,`I_SIGNED_TYPE);
+        $display("ADDI test passed!");
 
         // SLTI
+        OPCODE = `I_TYPE_OPCODE;
+        FUNCT3 = 3'b010;
+        FUNCT7 = 7'bxxxxxxx;
+        `DECODE_DELAY
+        `assert(OP1SEL,`DATA1);
+        `assert(OP2SEL,`IMM);
+        `assert(MEM_WRITE,`MEM_WRITE_0);
+        `assert(MEM_READ,`MEM_READ_0);
+        `assert(REG_WRITE_EN,`REG_WRITE_EN_1);
+        `assert(WB_SEL,`ALU);
+        `assert(ALU_OP,`SLT);
+        `assert(BRANCH_JUMP,`NO);
+        `assert(IMM_SEL,`I_SIGNED_TYPE);
+        $display("SLTI test passed!");
+        
         // SLTIU
+        OPCODE = `I_TYPE_OPCODE;
+        FUNCT3 = 3'b011;
+        FUNCT7 = 7'bxxxxxxx;
+        `DECODE_DELAY
+        `assert(OP1SEL,`DATA1);
+        `assert(OP2SEL,`IMM);
+        `assert(MEM_WRITE,`MEM_WRITE_0);
+        `assert(MEM_READ,`MEM_READ_0);
+        `assert(REG_WRITE_EN,`REG_WRITE_EN_1);
+        `assert(WB_SEL,`ALU);
+        `assert(ALU_OP,`SLTU);
+        `assert(BRANCH_JUMP,`NO);
+        `assert(IMM_SEL,`I_UNSIGNED_TYPE);
+        $display("SLTIU test passed!");
+
         // XORI
+        OPCODE = `I_TYPE_OPCODE;
+        FUNCT3 = 3'b100;
+        FUNCT7 = 7'bxxxxxxx;
+        `DECODE_DELAY
+        `assert(OP1SEL,`DATA1);
+        `assert(OP2SEL,`IMM);
+        `assert(MEM_WRITE,`MEM_WRITE_0);
+        `assert(MEM_READ,`MEM_READ_0);
+        `assert(REG_WRITE_EN,`REG_WRITE_EN_1);
+        `assert(WB_SEL,`ALU);
+        `assert(ALU_OP,`XOR);
+        `assert(BRANCH_JUMP,`NO);
+        `assert(IMM_SEL,`I_SIGNED_TYPE);
+        $display("XORI test passed!");
+
         // ORI
+        OPCODE = `I_TYPE_OPCODE;
+        FUNCT3 = 3'b110;
+        FUNCT7 = 7'bxxxxxxx;
+        `DECODE_DELAY
+        `assert(OP1SEL,`DATA1);
+        `assert(OP2SEL,`IMM);
+        `assert(MEM_WRITE,`MEM_WRITE_0);
+        `assert(MEM_READ,`MEM_READ_0);
+        `assert(REG_WRITE_EN,`REG_WRITE_EN_1);
+        `assert(WB_SEL,`ALU);
+        `assert(ALU_OP,`OR);
+        `assert(BRANCH_JUMP,`NO);
+        `assert(IMM_SEL,`I_SIGNED_TYPE);
+        $display("ORI test passed!");
+
         // ANDI
+        OPCODE = `I_TYPE_OPCODE;
+        FUNCT3 = 3'b111;
+        FUNCT7 = 7'bxxxxxxx;
+        `DECODE_DELAY
+        `assert(OP1SEL,`DATA1);
+        `assert(OP2SEL,`IMM);
+        `assert(MEM_WRITE,`MEM_WRITE_0);
+        `assert(MEM_READ,`MEM_READ_0);
+        `assert(REG_WRITE_EN,`REG_WRITE_EN_1);
+        `assert(WB_SEL,`ALU);
+        `assert(ALU_OP,`AND);
+        `assert(BRANCH_JUMP,`NO);
+        `assert(IMM_SEL,`I_SIGNED_TYPE);
+        $display("ANDI test passed!");
+        
         // SLLI
+        OPCODE = `I_TYPE_OPCODE;
+        FUNCT3 = 3'b001;
+        FUNCT7 = 7'b0000000;
+        `DECODE_DELAY
+        `assert(OP1SEL,`DATA1);
+        `assert(OP2SEL,`IMM);
+        `assert(MEM_WRITE,`MEM_WRITE_0);
+        `assert(MEM_READ,`MEM_READ_0);
+        `assert(REG_WRITE_EN,`REG_WRITE_EN_1);
+        `assert(WB_SEL,`ALU);
+        `assert(ALU_OP,`SLL);
+        `assert(BRANCH_JUMP,`NO);
+        `assert(IMM_SEL,`I_SHIFT_TYPE);
+        $display("SLLI test passed!");
+
         // SRLI
+        OPCODE = `I_TYPE_OPCODE;
+        FUNCT3 = 3'b101;
+        FUNCT7 = 7'b0000000;
+        `DECODE_DELAY
+        `assert(OP1SEL,`DATA1);
+        `assert(OP2SEL,`IMM);
+        `assert(MEM_WRITE,`MEM_WRITE_0);
+        `assert(MEM_READ,`MEM_READ_0);
+        `assert(REG_WRITE_EN,`REG_WRITE_EN_1);
+        `assert(WB_SEL,`ALU);
+        `assert(ALU_OP,`SRL);
+        `assert(BRANCH_JUMP,`NO);
+        `assert(IMM_SEL,`I_SHIFT_TYPE);
+        $display("SRLI test passed!");
+
         // SRAI
+        OPCODE = `I_TYPE_OPCODE;
+        FUNCT3 = 3'b101;
+        FUNCT7 = 7'b0100000;
+        `DECODE_DELAY
+        `assert(OP1SEL,`DATA1);
+        `assert(OP2SEL,`IMM);
+        `assert(MEM_WRITE,`MEM_WRITE_0);
+        `assert(MEM_READ,`MEM_READ_0);
+        `assert(REG_WRITE_EN,`REG_WRITE_EN_1);
+        `assert(WB_SEL,`ALU);
+        `assert(ALU_OP,`SRA);
+        `assert(BRANCH_JUMP,`NO);
+        `assert(IMM_SEL,`I_SHIFT_TYPE);
+        $display("SRAI test passed!");
+
 
         // ADD
         OPCODE = `R_TYPE_OPCODE;
